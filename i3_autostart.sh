a3ad5d936e320fb6043e15ad4e3b0893ea3e1ea0
kar18 <kazi-ar-rafi@protonmail.com>
2023-03-17 16:33:08 +0600

    if you run restart_to_extend_time.sh it will ectend time

--- i3_autostart.sh
+++ i3_autostart.sh
@@ -4,11 +4,14 @@
 dirpath=$(realpath "$(dirname "$0")")
 
 # Construct the exec command with the full path to the script
-exec_command="exec --no-startup-id '$dirpath/run.sh'"
+exec_command="exec --no-startup-id '$dirpath/run-break-timer.sh'"
+exec_command_two="bindsym Mod1+Mod4+period exec '$dirpath/restart_to_extend_time.sh'"
 
 # Check if the exec command exists in the i3 config file
 if ! grep -q "$exec_command" ~/.config/i3/config; then
+    echo "Adding"
     # If the command doesn't exist, add it to the end of the file
     echo "$exec_command" >> ~/.config/i3/config
+    echo "$exec_command_two" >> ~/.config/i3/config
 fi
-
+echo "Nothing to do, now"
